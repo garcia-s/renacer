1b813237909638c1eeeea443bb24f6e66bcdac5b
usuariopablodaniel <pablodaniel478@gmail.com>
2024-06-02 20:13:32 -0300

    Update config.sql
    Tabla user creada

--- install/config.sql
+++ install/config.sql
@@ -2,3 +2,9 @@ CREATE TABLE IF NOT EXISTS config (
     name VARCHAR(50),
     value VARCHAR(255)
 )
+CREATE TABLE user(
+    email: VARCHAR(50),
+    pasword: text,
+    salt: VARCHAR(200),
+    cycles: int
+)
