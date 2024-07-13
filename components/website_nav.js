b4145a6ed53a9387fdb493c61f0d96411961a5b9
Julio Cesar Solis <juliocsolis08@hotmail.com>
2024-07-12 22:57:06 -0300

    Styles in Nav
    No quedo lindo pero me salió hacer esto.

--- components/website_nav.js
+++ components/website_nav.js
@@ -2,15 +2,19 @@
 //PARA Pablito
 
 import Link from "next/link"
+import styles from "./website_nav.module.css"
 
 export default function WebNav() {
     return <header>
+
+        <div className={styles.navContainer}>
         <nav>
-            <ul>
+            <ul className={styles.ul}>
                 <li><Link href="/">Inicio</Link></li>
                 <li><Link href="./sobre-nosotros">Sobre Nosotros</Link></li>
                 <li><Link href="./nuestro-equipo">Nuestro Equipo</Link></li>
             </ul>
         </nav>
+        </div>
     </header>
 }
